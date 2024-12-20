5119fc8289c8e9640a86074351126c388b705051
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-12-21 08:54:52 +0900

    feat: 「ベクトルに色を足す」のような操作を行えないように修正

--- header/color.h
+++ header/color.h
@@ -2,32 +2,143 @@
 #define COLOR_H
 
 #include <iostream>
-#include "vec3.h"
 
-class Color : public VecBase
+class Color
 {
 public:
-    // コンストラクタは親クラスのものをそのまま利用
-    using VecBase::VecBase;
+    double r, g, b;
 
-    // r, g, b メソッド
-    double r() const { return v[0]; }
-    double g() const { return v[1]; }
-    double b() const { return v[2]; }
+    // コンストラクタ
+    Color() : r(.0), g(.0), b(.0) {}
+    Color(double _g) : r(_g), g(_g), b(_g) {}
+    Color(double _r, double _g, double _b) : r(_r), g(_g), b(_b) {}
 
-    // 代入演算
-    inline Color &operator=(const VecBase &vb)
+    // デストラクタ
+    ~Color() {}
+
+    // ベクトル同士の演算
+    inline Color operator+(const Color &c) const
     {
-        v[0] = vb.getV()[0];
-        v[1] = vb.getV()[1];
-        v[2] = vb.getV()[2];
+        return Color(r + c.r, g + c.g, b + c.b);
+    }
+
+    inline Color operator+=(const Color &c)
+    {
+        r += c.r, g += c.g, b += c.b;
         return *this;
+    }
+
+    inline Color operator-(const Color &c) const
+    {
+        return Color(r - c.r, g - c.g, b - c.b);
+    }
+
+    inline Color operator-=(const Color &c)
+    {
+        r -= c.r, g -= c.g, b -= c.b;
+        return *this;
+    }
+
+    inline Color operator*(const Color &c) const
+    {
+        return Color(r * c.r, g * c.g, b * c.b);
+    }
+
+    inline Color operator*=(const Color &c)
+    {
+        r *= c.r, g *= c.g, b *= c.b;
+        return *this;
+    }
+
+    inline Color operator/(const Color &c) const
+    {
+        return Color(r / c.r, g / c.g, b / c.b);
+    }
+
+    inline Color operator/=(const Color &c)
+    {
+        r /= c.r, g /= c.g, b /= c.b;
+        return *this;
+    }
+
+    // ベクトルとスカラーの演算
+    inline Color operator+(const double s) const
+    {
+        return Color(r + s, g + s, b + s);
+    }
+
+    inline friend Color operator+(const double s, const Color &c)
+    {
+        return Color(s + c.r, s + c.g, s + c.b);
+    }
+
+    inline Color operator+=(const double s)
+    {
+        r += s, g += s, b += s;
+        return *this;
+    }
+
+    inline Color operator-(const double s) const
+    {
+        return Color(r - s, g - s, b - s);
+    }
+
+    inline friend Color operator-(const double s, const Color &c)
+    {
+        return Color(s - c.r, s - c.g, s - c.b);
+    }
+
+    inline Color operator-=(const double s)
+    {
+        r -= s, g -= s, b -= s;
+        return *this;
+    }
+
+    inline Color operator*(const double s) const
+    {
+        return Color(r * s, g * s, b * s);
+    }
+
+    inline friend Color operator*(const double s, const Color &c)
+    {
+        return Color(s * c.r, s * c.g, s * c.b);
+    }
+
+    inline Color operator*=(const double s)
+    {
+        r *= s, g *= s, b *= s;
+        return *this;
+    }
+
+    inline Color operator/(const double s) const
+    {
+        return Color(r / s, g / s, b / s);
+    }
+
+    inline friend Color operator/(const double s, const Color &c)
+    {
+        return Color(s / c.r, s / c.g, s / c.b);
+    }
+
+    inline Color operator/=(const double s)
+    {
+        r /= s, g /= s, b /= s;
+        return *this;
+    }
+
+    // イコール演算子
+    inline bool operator==(const Color &c) const
+    {
+        if (r == c.r && g == c.g && b == c.b)
+            return true;
+        else
+            return false;
     }
 
     // コンソール出力
     inline friend std::ostream &operator<<(std::ostream &stream, const Color &c)
     {
-        stream << "(" << c.r() << ", " << c.g() << ", " << c.b() << ")";
+        stream << "(" << c.r << ", " << c.g << ", " << c.b << ")";
         return stream;
     }
 };
--- header/vec3.h
+++ header/vec3.h
@@ -4,191 +4,148 @@
 #include <iostream>
 #include <cmath>
 
-class VecBase
+class Vec3
 {
-protected:
-    double v[3];
-
 public:
+    double x, y, z;
+
     // コンストラクタ
-    VecBase() : v{.0, .0, .0} {}
-    VecBase(double _i) : v{_i, _i, _i} {}
-    VecBase(double _x, double _y, double _z) : v{_x, _y, _z} {}
+    Vec3() : x(.0), y(.0), z(.0) {}
+    Vec3(double _i) : x(_i), y(_i), z(_i) {}
+    Vec3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
 
     // デストラクタ
-    ~VecBase() {}
-
-    const double *getV() const { return v; }
-
-    // イコール演算子
-    inline bool operator==(const VecBase &vb) const
-    {
-        return v[0] == vb.v[0] && v[1] == vb.v[1] && v[2] == vb.v[2];
-    }
-
-    // マイナス演算
-    inline VecBase operator-() const
-    {
-        return VecBase(-v[0], -v[1], -v[2]);
-    }
+    ~Vec3() {}
 
     // ベクトル同士の演算
-    inline VecBase operator+(const VecBase &vb) const
+    inline Vec3 operator+(const Vec3 &v) const
     {
-        return VecBase(v[0] + vb.v[0], v[1] + vb.v[1], v[2] + vb.v[2]);
+        return Vec3(x + v.x, y + v.y, z + v.z);
     }
 
-    inline VecBase operator+=(const VecBase &vb)
+    inline Vec3 operator+=(const Vec3 &v)
     {
-        v[0] += vb.v[0];
-        v[1] += vb.v[1];
-        v[2] += vb.v[2];
+        x += v.x, y += v.y, z += v.z;
         return *this;
     }
 
-    inline VecBase operator-(const VecBase &vb) const
+    inline Vec3 operator-(const Vec3 &v) const
     {
-        return VecBase(v[0] - vb.v[0], v[1] - vb.v[1], v[2] - vb.v[2]);
+        return Vec3(x - v.x, y - v.y, z - v.z);
     }
 
-    inline VecBase operator-=(const VecBase &vb)
+    inline Vec3 operator-=(const Vec3 &v)
     {
-        v[0] -= vb.v[0];
-        v[1] -= vb.v[1];
-        v[2] -= vb.v[2];
+        x -= v.x, y -= v.y, z -= v.z;
         return *this;
     }
 
-    inline VecBase operator*(const VecBase &vb) const
+    inline Vec3 operator*(const Vec3 &v) const
     {
-        return VecBase(v[0] * vb.v[0], v[1] * vb.v[1], v[2] * vb.v[2]);
+        return Vec3(x * v.x, y * v.y, z * v.z);
     }
 
-    inline VecBase operator*=(const VecBase &vb)
+    inline Vec3 operator*=(const Vec3 &v)
     {
-        v[0] *= vb.v[0];
-        v[1] *= vb.v[1];
-        v[2] *= vb.v[2];
+        x *= v.x, y *= v.y, z *= v.z;
         return *this;
     }
 
-    inline VecBase operator/(const VecBase &vb) const
+    inline Vec3 operator/(const Vec3 &v) const
     {
-        return VecBase(v[0] / vb.v[0], v[1] / vb.v[1], v[2] / vb.v[2]);
+        return Vec3(x / v.x, y / v.y, z / v.z);
     }
 
-    inline VecBase operator/=(const VecBase &vb)
+    inline Vec3 operator/=(const Vec3 &v)
     {
-        v[0] /= vb.v[0];
-        v[1] /= vb.v[1];
-        v[2] /= vb.v[2];
+        x /= v.x, y /= v.y, z /= v.z;
         return *this;
     }
 
     // ベクトルとスカラーの演算
-    inline VecBase operator+(const double s) const
+    inline Vec3 operator+(const double s) const
     {
-        return VecBase(v[0] + s, v[1] + s, v[2] + s);
+        return Vec3(x + s, y + s, z + s);
     }
 
-    inline friend VecBase operator+(const double s, const VecBase &vb)
+    inline friend Vec3 operator+(const double s, const Vec3 &v)
     {
-        return VecBase(s + vb.v[0], s + vb.v[1], s + vb.v[2]);
+        return Vec3(s + v.x, s + v.y, s + v.z);
     }
 
-    inline VecBase operator+=(const double s)
+    inline Vec3 operator+=(const double s)
     {
-        v[0] += s;
-        v[1] += s;
-        v[2] += s;
+        x += s, y += s, z += s;
         return *this;
     }
 
-    inline VecBase operator-(const double s) const
+    inline Vec3 operator-(const double s) const
     {
-        return VecBase(v[0] - s, v[1] - s, v[2] - s);
+        return Vec3(x - s, y - s, z - s);
     }
 
-    inline friend VecBase operator-(const double s, const VecBase &vb)
+    inline friend Vec3 operator-(const double s, const Vec3 &v)
     {
-        return VecBase(s - vb.v[0], s - vb.v[1], s - vb.v[2]);
+        return Vec3(s - v.x, s - v.y, s - v.z);
     }
 
-    inline VecBase operator-=(const double s)
+    inline Vec3 operator-=(const double s)
     {
-        v[0] -= s;
-        v[1] -= s;
-        v[2] -= s;
+        x -= s, y -= s, z -= s;
         return *this;
     }
 
-    inline VecBase operator*(const double s) const
+    inline Vec3 operator*(const double s) const
     {
-        return VecBase(v[0] * s, v[1] * s, v[2] * s);
+        return Vec3(x * s, y * s, z * s);
     }
 
-    inline friend VecBase operator*(const double s, const VecBase &vb)
+    inline friend Vec3 operator*(const double s, const Vec3 &v)
     {
-        return VecBase(s * vb.v[0], s * vb.v[1], s * vb.v[2]);
+        return Vec3(s * v.x, s * v.y, s * v.z);
     }
 
-    inline VecBase operator*=(const double s)
+    inline Vec3 operator*=(const double s)
     {
-        v[0] *= s;
-        v[1] *= s;
-        v[2] *= s;
+        x *= s, y *= s, z *= s;
         return *this;
     }
 
-    inline VecBase operator/(const double s) const
+    inline Vec3 operator/(const double s) const
     {
-        return VecBase(v[0] / s, v[1] / s, v[2] / s);
+        return Vec3(x / s, y / s, z / s);
     }
 
-    inline friend VecBase operator/(const double s, const VecBase &vb)
+    inline friend Vec3 operator/(const double s, const Vec3 &v)
     {
-        return VecBase(s / vb.v[0], s / vb.v[1], s / vb.v[2]);
+        return Vec3(s / v.x, s / v.y, s / v.z);
     }
 
-    inline VecBase operator/=(const double s)
+    inline Vec3 operator/=(const double s)
     {
-        v[0] /= s;
-        v[1] /= s;
-        v[2] /= s;
-        return *this;
-    }
-};
-
-class Vec3 : public VecBase
-{
-public:
-    // コンストラクタは親クラスのものをそのまま利用
-    using VecBase::VecBase;
-
-    // x, y, z メソッド
-    double x() const { return v[0]; }
-    double y() const { return v[1]; }
-    double z() const { return v[2]; }
-
-    // 代入演算
-    inline Vec3 &operator=(const VecBase &vb)
-    {
-        v[0] = vb.getV()[0];
-        v[1] = vb.getV()[1];
-        v[2] = vb.getV()[2];
+        x /= s, y /= s, z /= s;
         return *this;
     }
 
-    // 逆ベクトルを取得する関数
-    inline Vec3 inverse() const
+    // イコール演算子
+    inline bool operator==(const Vec3 &v) const
     {
-        return Vec3(-x(), -y(), -z());
+        if (x == v.x && y == v.y && z == v.z)
+            return true;
+        else
+            return false;
+    }
+
+    // マイナス演算
+    inline Vec3 operator-() const
+    {
+        return Vec3(-x, -y, -z);
     }
 
     // ノルム値
     double norm() const
     {
-        return std::sqrt(x() * x() + y() * y() + z() * z());
+        return std::sqrt(x * x + y * y + z * z);
     }
 
     // 正規化
@@ -196,30 +153,30 @@ public:
     {
         double norm = this->norm();
         return Vec3(
-            x() / norm,
-            y() / norm,
-            z() / norm);
+            x / norm,
+            y / norm,
+            z / norm);
     }
 
     // 内積
     inline friend double dot(const Vec3 &v1, const Vec3 &v2)
     {
-        return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
 
     // 外積
     inline friend Vec3 cross(const Vec3 &v1, const Vec3 &v2)
     {
         return Vec3(
-            v1.y() * v2.z() - v1.z() * v2.y(),
-            v1.z() * v2.x() - v1.x() * v2.z(),
-            v1.x() * v2.y() - v1.y() * v2.x());
+            v1.y * v2.z - v1.z * v2.y,
+            v1.z * v2.x - v1.x * v2.z,
+            v1.x * v2.y - v1.y * v2.x);
     }
 
     // コンソール出力
     inline friend std::ostream &operator<<(std::ostream &stream, const Vec3 &v)
     {
-        stream << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+        stream << "(" << v.x << ", " << v.y << ", " << v.z << ")";
         return stream;
     }
 };
