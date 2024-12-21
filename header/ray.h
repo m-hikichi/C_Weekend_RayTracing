da1c0900b102145cfdefb114cf098b15b713e72e
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-12-21 10:18:23 +0900

    feat: Rayクラスの実装

---
+++ header/ray.h
@@ -0,0 +1,41 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <iostream>
+#include "vec3.h"
+
+class Ray {
+    private:
+        // レイの始点座標
+        Vec3 origin;
+        // レイの方向
+        Vec3 direction;
+
+    public:
+        // コンストラクタ
+        // レイの方向ベクトルは，正規化（単位ベクトル）して格納
+        Ray(const Vec3& _origin, const Vec3& _direction) :
+            origin(_origin), direction(_direction.normalize()) {}
+
+        // ゲッター
+        Vec3 get_origin() const {
+            return origin;
+        }
+
+        Vec3 get_direction() const {
+            return direction;
+        }
+
+        // r = o + t * d
+        Vec3 operator() (double t) const {
+            return origin + t * direction;
+        }
+
+        // コンソール出力
+        inline friend std::ostream& operator<<(std::ostream& stream, const Ray& r) {
+            stream << "origin    : " << r.origin << "\ndirection : " << r.direction;
+            return stream;
+        }
+};
+
+#endif
