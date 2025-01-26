6a802335d63b1b6d559f6143b37ad9cdc31e66e9
m_hikichi <hijiki.mshr19980119@outlook.com>
2025-01-26 10:53:41 +0900

    test: Glassクラスのテストコードの追加

--- test/test_material.cpp
+++ test/test_material.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 #include "../header/material.h"
 
-// Lambertian クラスのテスト
+/**
+ * Lambertian クラス
+ */
 TEST(LambertianTest, SampleRay)
 {
     // アルベド（反射率）を設定
@@ -13,14 +15,14 @@ TEST(LambertianTest, SampleRay)
     Vec3 hit_normal(0.0, 1.0, 0.0); // 上向きの法線
 
     // 入力レイの設定
-    Ray injection_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 6));
+    Ray incident_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 6));
     // ヒット情報の設定
     Hit hit(10, hit_position, hit_normal, nullptr, true);
 
     for (int i = 0; i < 100; i++)
     {
         // 反射ベクトルを計算する
-        Ray reflect_ray = lambertian.sample_ray(injection_ray, hit);
+        Ray reflect_ray = lambertian.sample_ray(incident_ray, hit);
         // 入力レイの始点が反射レイの始点と一致するか確認
         EXPECT_EQ(reflect_ray.get_origin(), hit_position);
         // 反射ベクトルの方向が期待通りか確認
@@ -58,7 +60,9 @@ TEST(LambertianTest, GetBRDFPointer)
     delete lambertian;
 }
 
-// Mirror クラスのテスト
+/**
+ * Mirror クラス
+ */
 TEST(MirrorTest, SampleRay)
 {
     // アルベド（反射率）を設定
@@ -70,12 +74,12 @@ TEST(MirrorTest, SampleRay)
     Vec3 hit_normal(0.0, 1.0, 0.0); // 上向きの法線
 
     // 入力レイの設定
-    Ray injection_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 6));
+    Ray incident_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 6));
     // ヒット情報の設定
     Hit hit(10, hit_position, hit_normal, nullptr, true);
 
     // 反射ベクトルを計算する
-    Ray reflect_ray = mirror.sample_ray(injection_ray, hit);
+    Ray reflect_ray = mirror.sample_ray(incident_ray, hit);
     // 入力レイの始点が反射レイの始点と一致するか確認
     EXPECT_EQ(reflect_ray.get_origin(), hit_position);
     // 反射ベクトルの方向が期待通りか確認
@@ -112,6 +116,112 @@ TEST(MirrorTest, GetBRDFPointer)
 
     // メモリ解放
     delete mirror;
+}
+
+/**
+ * Glass クラス
+ */
+TEST(GlassTest, SampleRayFromOutside)
+{
+    // アルベド（反射率）を設定
+    double refractive_index = 1.5;
+    Glass glass(refractive_index);
+
+    // ヒット位置と法線の設定
+    Vec3 hit_position(1.0, 2.0, 3.0);
+    Vec3 hit_normal(0.0, 1.0, 0.0); // 上向きの法線
+
+    // 入力レイの設定
+    Ray incident_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 2));
+    // ヒット情報の設定
+    Hit hit(10, hit_position, hit_normal, nullptr, true);
+
+    // 透過ベクトルを計算する
+    Ray reflect_ray = glass.sample_ray(incident_ray, hit);
+    // 入力レイの始点が透過レイの始点と一致するか確認
+    EXPECT_EQ(reflect_ray.get_origin(), hit_position);
+    // 透過ベクトルの方向が期待通りか確認
+    double eta_theta = asin(sin(M_PI / 3) * (1.0 / refractive_index));
+    EXPECT_NEAR(reflect_ray.get_direction().x, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).x, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().y, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).y, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().z, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).z, 1e-6);
+}
+
+TEST(GlassTest, SampleRayFromInside)
+{
+    // アルベド（反射率）を設定
+    double refractive_index = 1.5;
+    Glass glass(refractive_index);
+
+    // ヒット位置と法線の設定
+    Vec3 hit_position(1.0, 2.0, 3.0);
+    Vec3 hit_normal(0.0, -1.0, 0.0); // 下向きの法線
+
+    // 入力レイの設定
+    Ray incident_ray(hit_position, -spherical_to_cartesian(M_PI / 6, M_PI / 2));
+    // ヒット情報の設定
+    Hit hit(10, hit_position, hit_normal, nullptr, false);
+
+    // 透過ベクトルを計算する
+    Ray reflect_ray = glass.sample_ray(incident_ray, hit);
+    // 入力レイの始点が透過レイの始点と一致するか確認
+    EXPECT_EQ(reflect_ray.get_origin(), hit_position);
+    // 透過ベクトルの方向が期待通りか確認
+    double eta_theta = asin(sin(M_PI / 6) * (refractive_index / 1.0));
+    EXPECT_NEAR(reflect_ray.get_direction().x, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).x, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().y, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).y, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().z, spherical_to_cartesian(eta_theta + M_PI, M_PI / 2).z, 1e-6);
+}
+
+TEST(GlassTest, SampleRayTotalReflection)
+{
+    // アルベド（反射率）を設定
+    double refractive_index = 1.5;
+    Glass glass(refractive_index);
+
+    // ヒット位置と法線の設定
+    Vec3 hit_position(1.0, 2.0, 3.0);
+    Vec3 hit_normal(0.0, 1.0, 0.0); // 上向きの法線
+
+    // 入力レイの設定
+    Ray incident_ray(hit_position, -spherical_to_cartesian(M_PI / 3, M_PI / 2));
+    // ヒット情報の設定
+    Hit hit(10, hit_position, hit_normal, nullptr, false);
+
+    // 反射ベクトルを計算する
+    Ray reflect_ray = glass.sample_ray(incident_ray, hit);
+    // 入力レイの始点が反射レイの始点と一致するか確認
+    EXPECT_EQ(reflect_ray.get_origin(), hit_position);
+    // 反射ベクトルの方向が期待通りか確認
+    // 反射ベクトルは入力レイの方向に180度回転したもの
+    EXPECT_NEAR(reflect_ray.get_direction().x, spherical_to_cartesian(M_PI / 3, M_PI / 2 + M_PI).x, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().y, spherical_to_cartesian(M_PI / 3, M_PI / 2 + M_PI).y, 1e-6);
+    EXPECT_NEAR(reflect_ray.get_direction().z, spherical_to_cartesian(M_PI / 3, M_PI / 2 + M_PI).z, 1e-6);
+}
+
+TEST(GlassTest, GetBRDFDirect)
+{
+    // Glassオブジェクトを直接作成
+    double refractive_index = 1.5;
+    Glass glass(refractive_index);
+
+    // get_brdf がアルベドを返すか確認
+    Color brdf = glass.get_brdf();
+    EXPECT_EQ(brdf, Color(1.0));
+}
+
+TEST(GlassTest, GetBRDFPointer)
+{
+    // Materialポインタを使ってGlassオブジェクトを作成
+    double refractive_index = 1.5;
+    Material *glass = new Glass(refractive_index);
+
+    // get_brdf がアルベドを返すか確認
+    Color brdf = glass->get_brdf();
+    EXPECT_EQ(brdf, Color(1.0));
+
+    // メモリ解放
+    delete glass;
 }
 
 // メイン関数（Google Testのエントリーポイント）
