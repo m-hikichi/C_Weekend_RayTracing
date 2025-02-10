67e97417bd06715e1253cfd89dbde66bd52b14ec
m_hikichi <hijiki.mshr19980119@outlook.com>
2025-02-10 12:50:18 +0900

    feat: カメラの位置移動とカメラの向きを変更できるように修正

--- header/camera.h
+++ header/camera.h
@@ -8,26 +8,36 @@
 class Camera
 {
 private:
-    const double vertical_field_of_view; // 垂直方向の視野角（弧度法）
     const Image image;
-    Vec3 origin;
+    const Ray view_direction;
+    const double vertical_field_of_view; // 垂直方向の視野角（弧度法）
     Vec3 horizon;
     Vec3 vertical;
     Vec3 left_lower_corner;
 
 public:
-    Camera(int _image_width, int _image_height, const double _vertical_fov = M_PI / 2)
-        : image(Image(_image_width, _image_height)), vertical_field_of_view(_vertical_fov)
+    Camera(
+        const int _image_width,
+        const int _image_height,
+        const Ray _view_direction,
+        const double _vertical_fov = M_PI / 2)
+        : image(Image(_image_width, _image_height)),
+          view_direction(_view_direction),
+          vertical_field_of_view(_vertical_fov)
     {
         const double aspect_ratio = double(_image_width) / double(_image_height);
         const double viewport_height = 2.0 * tan(vertical_field_of_view / 2);
         const double viewport_width = aspect_ratio * viewport_height;
-        const double focal_length = 1.0;
 
-        origin = Vec3(0);
-        horizon = Vec3(viewport_width, 0, 0);
-        vertical = Vec3(0, viewport_height, 0);
-        left_lower_corner = origin - horizon / 2 - vertical / 2 - Vec3(0, 0, focal_length);
+        // カメラの向きを表す正規直交規定(u, v, w)の定義
+        const Vec3 view_up = Vec3(0, 1, 0);
+        const Vec3 w = view_direction.get_direction();
+        const Vec3 u = cross(view_up, w).normalize();
+        const Vec3 v = cross(w, u).normalize();
+
+        horizon = viewport_width * u;
+        vertical = viewport_height * v;
+        left_lower_corner = view_direction.get_origin() - horizon / 2 - vertical / 2 - w;
     }
 
     Image get_image() const
@@ -45,7 +55,7 @@ public:
         // アンチエイリアシングを行うために、ピクセル内のランダムな地点を通るサンプルの生成
         double u = (double(pixel_x) + generate_random_in_range(0.0, 1.0)) / double(image.get_width());
         double v = 1.0 - (double(pixel_y) + generate_random_in_range(0.0, 1.0)) / double(image.get_height());
-        return Ray(origin, left_lower_corner + u * horizon + v * vertical - origin);
+        return Ray(view_direction.get_origin(), left_lower_corner + u * horizon + v * vertical - view_direction.get_origin());
     }
 };
 
