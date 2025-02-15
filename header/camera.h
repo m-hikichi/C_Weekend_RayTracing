e590799dd77dff5bb66bb4e340db8a1cc15a1ede
m_hikichi <hijiki.mshr19980119@outlook.com>
2025-02-15 13:50:58 +0900

    feat: レンズカメラの実装

--- header/camera.h
+++ header/camera.h
@@ -72,4 +72,57 @@ public:
     }
 };
 
+
+class ThinLensCamera : public Camera
+{
+private:
+    const double lens_radius; // レンズの絞り（値が小さいほどピントの合う範囲が広くなり、値が大きいほどピントの合う範囲が狭くなりボケが発生する）
+
+public:
+    ThinLensCamera(
+        const int _image_width,
+        const int _image_height,
+        const Ray _view_direction,
+        const double _aperture,
+        const double _focus_distance,
+        const double _vertical_fov = M_PI / 2)
+        : Camera(_image_width, _image_height, _view_direction, _vertical_fov),
+          lens_radius(_aperture / 2)
+    {
+        const double aspect_ratio = double(_image_width) / double(_image_height);
+        const double viewport_height = 2.0 * tan(vertical_field_of_view / 2);
+        const double viewport_width = aspect_ratio * viewport_height;
+
+        // カメラの向きを表す正規直交規定(u, v, w)の定義
+        const Vec3 view_up = Vec3(0, 1, 0);
+        const Vec3 w = -view_direction.get_direction();
+        const Vec3 u = cross(view_up, w).normalize();
+        const Vec3 v = cross(w, u).normalize();
+
+        horizon = _focus_distance * viewport_width * u;
+        vertical = _focus_distance * viewport_height * v;
+        left_lower_corner = view_direction.get_origin() - horizon / 2 - vertical / 2 - _focus_distance * w;
+    }
+
+    Ray get_ray(const int pixel_x, const int pixel_y) const override
+    {
+        // レンズから放たれるレイ
+        Vec3 lens_sample = lens_radius * (Vec3(generate_random_in_range(.0, 1.0), generate_random_in_range(.0, 1.0), 0));
+
+        // カメラの向きを表す正規直交規定(u, v, w)の定義
+        const Vec3 view_up = Vec3(0, 1, 0);
+        const Vec3 w = -view_direction.get_direction();
+        const Vec3 u = cross(view_up, w).normalize();
+        const Vec3 v = cross(w, u).normalize();
+        Vec3 offset = lens_sample.x * u + lens_sample.y * v;
+
+        // アンチエイリアシングを行うために、ピクセル内のランダムな地点を通るサンプルの生成
+        double s = (double(pixel_x) + generate_random_in_range(.0, 1.0)) / double(image.get_width());
+        double t = 1.0 - (double(pixel_y) + generate_random_in_range(.0, 1.0)) / double(image.get_height());
+        return Ray(
+            view_direction.get_origin() + offset,
+            left_lower_corner + s * horizon + t * vertical - view_direction.get_origin() - offset);
+    }
+};
+
 #endif
